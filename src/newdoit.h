e0cd8608499a0fa47e578b453b79174295a7df21
Manfred Brath <manfred.brath@uni-hamburg.de>
2019-09-06 15:20:15 +0200

    Start of newdoit-project

---
+++ src/newdoit.h
@@ -0,0 +1,73 @@
+/* Copyright (C) 2019
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+   USA.
+*/
+
+/*!
+  \file   newdoit.cc
+  \author Manfred Brath manfred.brath@uni-hamburg.de
+  \date   06/09/2019
+  
+  \brief  This file will contain the internal functions
+          to calculate the radiative transfer
+          inside the cloudbox using the newDoit method.
+  
+*/
+
+/*===========================================================================
+  === External declarations
+  ===========================================================================*/
+
+#include "doit.h"
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+#include "agenda_class.h"
+#include "array.h"
+#include "auto_md.h"
+#include "check_input.h"
+#include "cloudbox.h"
+#include "geodetic.h"
+#include "lin_alg.h"
+#include "logic.h"
+#include "math_funcs.h"
+#include "matpackVII.h"
+#include "messages.h"
+#include "physics_funcs.h"
+#include "ppath.h"
+#include "propagationmatrix.h"
+#include "rte.h"
+#include "sorting.h"
+#include "special_interp.h"
+#include "xml_io.h"
+
+extern const Numeric PI;
+extern const Numeric RAD2DEG;
+
+void NewDoitMonoCalc(Workspace& ws,
+        // WS Input and Output:
+                              Tensor6& doit_i_field_mono,
+
+        // WS Input:
+                              ConstVectorView f_grid,
+                              const Index& f_index,
+                              const Index& maximum_number_iterations,
+                              const Agenda& doit_rte_agenda,
+                              const Agenda& doit_conv_test_agenda,
+                              const Index& accelerated,
+                              const Verbosity& verbosity)
+
